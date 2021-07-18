8a1ea4c8c9d757d107af997a419587c55c2c47c1
Roman Baev <skultaurora@gmail.com>
2021-07-18 22:42:19 +0300

    Add ORG_DMYA (Day, Month, Year, Weekday) template

--- orgdaypage.py
+++ orgdaypage.py
@@ -91,6 +91,7 @@ def OnLoaded(view,dt):
         , "ORG_LINENUM":       str(view.curRow())
         , "ORG_FILENAME":      dayPageGetDateString(dt)
         , "ORG_AUTHOR":        getpass.getuser()
+        , "ORG_DATE_DMYA":     dt.date().today().strftime("%d.%m.%Y %A")
         })
     view.settings().set('auto_indent',ai)
 
